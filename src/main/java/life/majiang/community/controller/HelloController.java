16a1c10412448c252a155932870aeb7b16be83c8
Kong Weiqi <2326257561@qq.com>
2024-06-06 15:30:34 +0800

    init repo

---
+++ src/main/java/life/majiang/community/controller/HelloController.java
@@ -0,0 +1,16 @@
+package life.majiang.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+//@Controller定义一个控制器类
+@Controller
+public class HelloController {
+    //@GetMapping用于处理请求方法的GET类型
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name") String name, Model model){
+        model.addAttribute("name",name);
+        return "hello";
+    }
+}
